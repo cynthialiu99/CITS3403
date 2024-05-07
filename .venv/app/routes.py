8107a0fb213dfb603913a9cb008124e8d786d1fd
sdubey2024 <23066191@student.uwa.edu.au>
2024-05-07 13:30:05 +0800

    Updated flask code
    Updated init.py, forms.py, models.py and routes.py files. Need to test files by running flask.

--- .venv/app/routes.py
+++ .venv/app/routes.py
@@ -1,35 +1,56 @@
 import sqlite3
 from flask import redirect, render_template, request, url_for, jsonify
-from app import flaskApp
-from app.models import Student, Staff
+from urllib.parse import urlsplit
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db, flaskApp
+from app.forms import SignUp, LoginForm, flash
+from app.models import User
+from flask_login import logout_user, login_required
 
 @flaskApp.route("/")
-
-@flaskApp.route('/signup')
-def signup():
-    return render_template('Sign Up.html')
-
-@flaskApp.route('/signup/submit', methods=['POST'])
-def submit():
-    print(request.method)
-    print(request.form)
-    print("Submitted!")
-    return redirect(location="/login")
-
-@flaskApp.route('/login', methods=['GET'])
-def login():
-    return redirect(location="/login/submit")
-
-@flaskApp.route('/login/submit', methods=['POST'])
-def submit2():
-    print(request.method)
-    print(request.form)
-    print("Submitted!")
-    return redirect(location="/account")
-
-@flaskApp.route('/account', methods=['GET'])
+@flaskApp.route('/account')
+@login_required
 def account():
-    return render_template("Account.html")
+    return render_template("Account.html", title='Account Page')
+
+@flaskApp.route('/signup', methods=['GET','POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    
+    form = SignUp()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('Sign Up.html', title='Sign Up', form=form)
+
+@flaskApp.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('Login.html', title='Sign In', form=form)
+
+@flaskApp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 # Route to create a new thread
 @flaskApp.route('/threads', methods=['POST'])
@@ -52,6 +73,7 @@ def get_threads():
     conn.close()
     return jsonify(threads), 200
 
+#Route to reply to threads
 @flaskApp.route('/threads/<thread_id>/reply', methods=['GET'])
 def reply_threads():
     return render_template("Reply_Threads.html")
