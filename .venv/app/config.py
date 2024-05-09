8900eda43fc8cbdec856f45d0d4c818566a0f574
Neha <=>
2024-05-09 15:03:48 +0800

    wtf forms modification

--- .venv/app/config.py
+++ .venv/app/config.py
@@ -1,7 +1,10 @@
 import os
+from flask import Flask
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+flaskApp = Flask(__name__)
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'potatoes-are-the-best'
