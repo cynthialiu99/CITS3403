99e4136f5f3bc8e6d3bfb5612ebef527dbb84c5d
nehamenon139 <122662919+nehamenon139@users.noreply.github.com>
2024-05-14 20:54:17 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,4 +7,4 @@ flaskApp = Flask(__name__)
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'potatoes-are-the-best'
+    SECRET_KEY = 'potatoes-are-the-best'
