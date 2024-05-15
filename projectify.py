c2a57454a5160e86841c6a712ece4c17a617eba0
sdubey2024 <23066191@student.uwa.edu.au>
2024-05-15 11:46:12 +0800

    Update projectify.py
    Added flask_migrate to code

--- projectify.py
+++ projectify.py
@@ -1,1 +1,6 @@
-from app import flaskApp
+from app import flaskApp, create_app, db
+from app.config import DeploymentConfig
+from flask_migrate import Migrate
+
+flaskApp = create_app(DeploymentConfig)
+migrate = Migrate(db, flaskApp)
