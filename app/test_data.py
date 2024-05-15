14070a4d07196027c3de6600d8c45c9a66fc9762
sdubey2024 <23066191@student.uwa.edu.au>
2024-05-15 16:43:58 +0800

    Update test_data.py
    updated test_data python code, added new function to add test data to database

--- app/test_data.py
+++ app/test_data.py
@@ -1,11 +1,21 @@
-from app import db
-from app.models import *
+from app import create_app, db
+from app.app import flaskApp
+from app.models import User
 
+# Function to add test data to the database
+def add_test_data_to_db():
+    with flaskApp.app_context():
+        test_data = [
+            User(student_id= "01349324", name="Tom K", email="01349324@student.uwa.edu.au"),
+            User(student_id = "01349523", name="Jerome D", email="01349523@student.uwa.edu.au"), 	
+            User(staff_id = "01349622", name="Cardi B", email="01349622@uwa.edu.au"),  	
+            User(staff_id = "01349721", name="Taylor K", email="01349721@uwa.edu.au")  
+        ]
+        db.session.bulk_save_objects(test_data)
+        db.session.commit()
+        print("Test data added to the database.")
 
-tom = User(uwa_id= "01349324", name="Tom K", email="01349324@student.uwa.edu.au")
-jerome = User(uwa_id = "01349523", name="Jerome D", email="01349523@student.uwa.edu.au")  	
-cardi = User(staff_id = "01349622", name="Cardi B", email="01349622@uwa.edu.au")  	
-taylor = User(staff_id = "01349721", name="Taylor K", email="01349721@uwa.edu.au")  
+# Run the function to add test data
+if __name__ == "__main__":
+    add_test_data_to_db()
 
-db.session.add_all([tom, jerome, cardi, taylor])
-db.session.commit()
