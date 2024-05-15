4d7136b72e803b5f6cc5ab7392d73761146a8508
sdubey2024 <23066191@student.uwa.edu.au>
2024-05-15 16:44:27 +0800

    Update __init__.py
    fixed python code in __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,34 @@
+import os
 from flask import Flask
-from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-flaskApp = Flask(__name__)
-flaskApp.config.from_object(Config)
-
-db = SQLAlchemy(flaskApp)
-migrate = Migrate(flaskApp, db)
-login = LoginManager(flaskApp)
-login.login_view = 'login'
-
-
-
 db = SQLAlchemy()
-def create_app(config):
+migrate = Migrate()
+login = LoginManager()
+
+def create_app():
     flaskApp = Flask(__name__)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    class Config:
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+        SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "my_secret_key")
+        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
+
     flaskApp.config.from_object(Config)
-    #intialise routes
+    
     db.init_app(flaskApp)
-
-    from app.blueprint import main
-    flaskApp.register_blueprint(main)
-
-
+    migrate.init_app(flaskApp, db)
+    login.init_app(flaskApp)
+    login.login_view = 'auth.login'
+    
+    with flaskApp.app_context():
+        from . import models  # Import models to register them with SQLAlchemy
+        db.create_all()  # Create the database tables
+    
+    from .blueprint import main as main_blueprint
+    flaskApp.register_blueprint(main_blueprint)
+    
     return flaskApp
-
-from app import models, routes
