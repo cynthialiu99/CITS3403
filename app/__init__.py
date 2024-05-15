495910d4f3e39223597a8e0a43c4f5eee76b81bc
sdubey2024 <23066191@student.uwa.edu.au>
2024-05-15 11:44:16 +0800

    Update __init__.py
    added create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,25 @@ from flask_login import LoginManager
 
 flaskApp = Flask(__name__)
 flaskApp.config.from_object(Config)
+
 db = SQLAlchemy(flaskApp)
 migrate = Migrate(flaskApp, db)
 login = LoginManager(flaskApp)
 login.login_view = 'login'
+
+
+
+db = SQLAlchemy()
+def create_app(config):
+    flaskApp = Flask(__name__)
+    flaskApp.config.from_object(Config)
+    #intialise routes
+    db.init_app(flaskApp)
+
+    from app.blueprint import main
+    flaskApp.register_blueprint(main)
+
+
+    return flaskApp
 
 from app import models, routes
