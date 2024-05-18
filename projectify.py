09baaf79878454334addf09f324dc23533ded828
nehamenon139 <122662919+nehamenon139@users.noreply.github.com>
2024-05-18 19:18:05 +0800

    Modified projectify.py to ensure exporting env variable works

--- projectify.py
+++ projectify.py
@@ -1,6 +1,9 @@
-from app import flaskApp, create_app, db
+from app import create_app, db
 from app.config import DeploymentConfig
 from flask_migrate import Migrate
 
 flaskApp = create_app(DeploymentConfig)
-migrate = Migrate(db, flaskApp)
+migrate = Migrate(flaskApp,db)
+
+if __name__ == "__main__":
+    flaskApp.run()
